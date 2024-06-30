05dd9609518cdd063db34ad7fc5de17d0b3f8b1e
realharshita <slayyharshita@gmail.com>
2024-06-30 10:57:30 +0530

    added sliding and joining

--- tzfe.py
+++ tzfe.py
@@ -16,8 +16,24 @@ def add_random_tile(board):
         i, j = random.choice(empty_positions)
         board[i][j] = random.choice([2, 4])
 
+def move_left(board):
+    new_board = []
+    for row in board:
+        new_row = [num for num in row if num != 0]
+        for i in range(len(new_row) - 1):
+            if new_row[i] == new_row[i + 1]:
+                new_row[i] *= 2
+                new_row[i + 1] = 0
+        new_row = [num for num in new_row if num != 0]
+        new_row += [0] * (BOARD_SIZE - len(new_row))
+        new_board.append(new_row)
+    return new_board
+
 if __name__ == "__main__":
     game_board = initialize_board()
     add_random_tile(game_board)
     add_random_tile(game_board)
+    display_board(game_board)
+    print("\nAfter moving left:\n")
+    game_board = move_left(game_board)
     display_board(game_board)
